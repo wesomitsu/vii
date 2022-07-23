51597516cb6799207f9b231db7a4ad8614309de2
wesomitsu <wathek.z@hotmail.com>
2022-07-23 21:52:52 +0300

    Heroku config

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const port = process.env.PORT || 5000
 
 
 //app.use(express.static('public')); // this wont work
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
-app.use('/dist', express.static(__dirname + '/dist'));
+app.use('public', express.static(__dirname + 'public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
