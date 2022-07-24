f11a5704afdaf3468a2a24bf5c9f6d6aa96656b3
wesomitsu <wathek.z@hotmail.com>
2022-07-24 19:23:05 +0300

    Heroku config

--- public/js/app.3f6003b8.js
+++ public/js/app.3f6003b8.js
@@ -1129,7 +1129,7 @@
 					}, [t._v(" read more ")],), a("button", {
                         staticClass: "btn-sm btn btn-outline-secondary no-outline mw-4",
                         attrs: {
-                            href: ""+e.visit+""
+                            href: "#"
                         },staticStyle: {
 							height: "31px",
 							"margin-top": "5px"
