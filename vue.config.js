019e3c1d512686f08fc8acae85da1184f8b44ecf
wesomitsu <wathek.z@hotmail.com>
2022-12-05 02:43:05 +0300

    d

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/vii/'
-      : '/'
-  }
-
-// module.exports = {
-//     puplicPath: '/vii/',
-//     chainWebpack: config => {
-//         config.module.rules.delete('eslint');
-//     }
-// }
+    ? '/vii/'
+    : '/'
+}
