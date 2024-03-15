b1cd8de5a4c0e185da9ae73318abb3a1e687afac
Wathek Al Zoubi <wathek.z@hotmail.com>
2024-03-15 23:16:26 +0300

    Update app.3f6003b8.js

--- public/js/app.3f6003b8.js
+++ public/js/app.3f6003b8.js
@@ -380,7 +380,7 @@
                         blur: !1
                     }
                 },
-                description: "Hey there, my name is Wathek Al Zoubi I'm a Technical Product Manager for 2+ experience & CRO/Frontend Developer with 7+ experience. I am also a WordPress development expert. I fully embraced the Gutenberg/Elementor editor and can create custom blocks from scratch for any conceivable need.",
+                description: "Hey there, my name is Wathek Al Zoubi I'm a Creative Developer with over 9 years of combined experience in CRO/Product Development. I am also a WordPress expert. I fully embraced the Gutenberg/Elementor editor and can create custom blocks from scratch for any conceivable need.",
                 links: {
                     linkedin: "https://www.linkedin.com/in/wathek-alzoubi/",
                     github: "https://github.com/wesomitsu",
